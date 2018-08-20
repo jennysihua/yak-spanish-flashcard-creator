524cc1b24959eff0a9fe24740ce0dd6a98a7c096
Jenny Wang <jennysihua@gmail.com>
2018-08-20 17:53:45 -0400

    add favicon

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {db} = require('./server/db')
 const app = require('./server')
 const PORT = process.env.PORT || 1337
 
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
+db.sync()
   .then(() => {
     console.log('db synced')
     app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
