44374a33dddc9c04bcf227a2c83ea3bc6fb89a67
Jenny Wang <jennysihua@gmail.com>
2018-08-20 17:32:54 -0400

    add DATABASE_URL

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,5 @@
 'use strict'
 
-// The sole purpose of this module is to establish a connection to your
-// Postgres database by creating a Sequelize instance (called `db`).
-// You shouldn't need to make any modifications here.
 
 const chalk = require('chalk')
 const Sequelize = require('sequelize')
@@ -10,9 +7,8 @@ const pkg = require('../../package.json')
 
 console.log(chalk.yellow('Opening database connection'))
 
-// create the database instance that can be used in other database files
-const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-  logging: false // so we don't see all the SQL queries getting made
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`, {
+  logging: false
 })
 
 module.exports = db
