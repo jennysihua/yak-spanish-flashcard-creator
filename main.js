28d6d27dc20ed770f959b68136392ef295c2c087
Jenny Wang <jennysihua@gmail.com>
2018-08-20 17:39:17 -0400

    add PORT

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const {db} = require('./server/db')
 const app = require('./server')
-const PORT = 1337
+const PORT = process.env.PORT || 1337
 
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
